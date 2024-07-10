c2306ec849b81c7f205ee1246b11e5d1461002f1
Anan-h <anan.hosien@gmail.com>
2024-07-10 11:52:56 +0300

    modeling base app page and login page

--- intro_to_selenium/open_library/logic/base_app_page.py
+++ intro_to_selenium/open_library/logic/base_app_page.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
+
 from intro_to_selenium.open_library.infra.base_page import BasePage
 
 
@@ -7,3 +10,22 @@ class BaseAppPage(BasePage):
     MY_BOOKS_LINK="//ul[@class='navigation-component']//a[@href='/account/books']"
     SEARCH_INPUT="//input[@name='q']"
     MENU="//img[@class='hamburger__icon']"
+    BROWSE_DROPDOWN="//ul[@class='navigation-component']//div[@class='browse-component header-dropdown']"
+    SEARCH_BY_SELECTOR="//select[@aria-label='Search by']"
+    SEARCH_ICON="//input[@class='search-bar-submit']"
+    RANDOM_BOOK_BUTTON="//ul[@class='navigation-component']//a[@href='/random']"
+    REPORT_PROBLEM_LINK="//a[text()='Report A Problem']"
+    LOG_IN_BUTTON="//a[@class='btn']"
+
+    def __init__(self,driver):
+        super().__init__(driver)
+        try:
+            self._logo=self._driver.find_element(By.XPATH,self.LOGO)
+            self._my_books_link=self._driver.find_element(By.XPATH,self.MY_BOOKS_LINK)
+            self._search_input=self._driver.find_element(By.XPATH,self.SEARCH_INPUT)
+            self._menu=self._driver.find_element(By.XPATH,self.MENU)
+            self._browse_drop_down=self._driver.find_element(By.XPATH,self.BROWSE_DROPDOWN)
+            self._search_by_selector=self._driver.find_element(By.XPATH,self.SEARCH_BY_SELECTOR)
+            self._search_icon=self._driver.find_element(By.XPATH,self.SEARCH_ICON)
+        except NoSuchElementException as e:
+            print(e)
--- intro_to_selenium/open_library/logic/login_page.py
+++ intro_to_selenium/open_library/logic/login_page.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from intro_to_selenium.open_library.infra.base_page import BasePage
 
@@ -7,12 +10,16 @@ class LoginPage(BasePage):
     EMAIL_INPUT = "//input[@id='username']"
     PASSWORD_INPUT = "//input[@id='password']"
     LOG_IN_BUTTON = "//button[@name='login']"
+    INVALID_EMAIL_MSG = "//div[@class='note']"
 
     def __init__(self, driver):
         super().__init__(driver)
-        self._email_input = self._driver.find_element(By.XPATH, self.EMAIL_INPUT)
-        self._password_input = self._driver.find_element(By.XPATH, self.PASSWORD_INPUT)
-        self._login_button = self._driver.find_element(By.XPATH, self.LOG_IN_BUTTON)
+        try:
+            self._email_input = self._driver.find_element(By.XPATH, self.EMAIL_INPUT)
+            self._password_input = self._driver.find_element(By.XPATH, self.PASSWORD_INPUT)
+            self._login_button = self._driver.find_element(By.XPATH, self.LOG_IN_BUTTON)
+        except NoSuchElementException as e:
+            print(e)
 
     def fill_email_input(self, email):
         self._email_input.send_keys(email)
@@ -27,3 +34,22 @@ class LoginPage(BasePage):
         self.fill_email_input(email)
         self.fill_password_input(password)
         self.click_on_login_button()
+
+    def error_msg_is_visible(self):
+        try:
+            msg = WebDriverWait(self._driver, 5).until(
+                EC.visibility_of_element_located((By.XPATH, self.INVALID_EMAIL_MSG))
+            )
+            return msg.is_displayed()
+        except NoSuchElementException as e:
+            print(e)
+
+    def get_error_msg_text(self):
+        try:
+            msg = WebDriverWait(self._driver, 5).until(
+                EC.visibility_of_element_located((By.XPATH, self.INVALID_EMAIL_MSG))
+            )
+            return msg.text
+        except NoSuchElementException as e:
+            print(e)
+
