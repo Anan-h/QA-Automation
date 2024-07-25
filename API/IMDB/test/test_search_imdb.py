38e04e77bd93b1dc9d835a021dc553acdc8adeca
Anan-h <anan.hosien@gmail.com>
2024-07-25 20:03:05 +0300

    undo wring changes

--- API/IMDB/test/test_search_imdb.py
+++ API/IMDB/test/test_search_imdb.py
@@ -15,20 +15,14 @@ class TestSearchIMDB(unittest.TestCase):
         self.jira_handler = JiraHandler()
 
     def tearDown(self):
-        if hasattr(self, '_outcome'):
-            errors = self._outcome.errors
-            for test, exc_info in errors:
-                if exc_info is not None:
-                    logging.error("Test failed: %s", self._testMethodName)
         logging.info("-----------------------------------------------------------------")
 
     def test_search_imdb_for_valid_movie_name(self):
         logging.info("testing the search function with valid input ")
         response = self.request.get_search_imdb_for_text(self.config["movie_name"])
-        # self.assertTrue(response.ok)
-        # self.assertEqual(response.status, self.config["status_code"])
-        # self.assertEqual(response.data["message"], self.config["good_message"])
-        self.assertTrue(False)
+        self.assertTrue(response.ok)
+        self.assertEqual(response.status, self.config["status_code"])
+        self.assertEqual(response.data["message"], self.config["good_message"])
 
     def test_search_imdb_for_invalid_movie_name(self):
         logging.info("testing the search function with invalid input")
